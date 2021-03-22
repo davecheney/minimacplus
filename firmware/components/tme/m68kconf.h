49f7829b3c6b16af37836e704f37fa97a725d2a1
Dave Cheney <dave@cheney.net>
2021-03-22 16:41:27 +1100

    switch it musashi upstream

--- firmware/components/tme/m68kconf.h
+++ firmware/components/tme/m68kconf.h
@@ -3,10 +3,10 @@
 /* ======================================================================== */
 /*
  *                                  MUSASHI
- *                                Version 3.4
+ *                                Version 3.32
  *
  * A portable Motorola M680x0 processor emulation engine.
- * Copyright 1998-2001 Karl Stenerud.  All rights reserved.
+ * Copyright Karl Stenerud.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -67,15 +67,15 @@
 #define M68K_EMULATE_010            OPT_OFF
 #define M68K_EMULATE_EC020          OPT_OFF
 #define M68K_EMULATE_020            OPT_OFF
+#define M68K_EMULATE_030            OPT_OFF
+#define M68K_EMULATE_040            OPT_OFF
 
 
 /* If ON, the CPU will call m68k_read_immediate_xx() for immediate addressing
  * and m68k_read_pcrelative_xx() for PC-relative addressing.
  * If off, all read requests from the CPU will be redirected to m68k_read_xx()
  */
-#define M68K_SEPARATE_READS         OPT_ON
-//#define M68K_SEPARATE_READS         OPT_OFF
-
+#define M68K_SEPARATE_READS         OPT_OFF
 
 /* If ON, the CPU will call m68k_write_32_pd() when it executes move.l with a
  * predecrement destination EA mode instead of m68k_write_32().
@@ -111,6 +111,35 @@
 #define M68K_EMULATE_RESET          OPT_OFF
 #define M68K_RESET_CALLBACK()       your_reset_handler_function()
 
+/* If ON, CPU will call the callback when it encounters a cmpi.l #v, dn
+ * instruction.
+ */
+#define M68K_CMPILD_HAS_CALLBACK     OPT_OFF
+#define M68K_CMPILD_CALLBACK(v,r)    your_cmpild_handler_function(v,r)
+
+
+/* If ON, CPU will call the callback when it encounters a rte
+ * instruction.
+ */
+#define M68K_RTE_HAS_CALLBACK       OPT_OFF
+#define M68K_RTE_CALLBACK()         your_rte_handler_function()
+
+/* If ON, CPU will call the callback when it encounters a tas
+ * instruction.
+ */
+#define M68K_TAS_HAS_CALLBACK       OPT_OFF
+#define M68K_TAS_CALLBACK()         your_tas_handler_function()
+
+/* If ON, CPU will call the callback when it encounters an illegal instruction
+ * passing the opcode as argument. If the callback returns 1, then it's considered
+ * as a normal instruction, and the illegal exception in canceled. If it returns 0,
+ * the exception occurs normally.
+ * The callback looks like int callback(int opcode)
+ * You should put OPT_SPECIFY_HANDLER here if you cant to use it, otherwise it will
+ * use a dummy default handler and you'll have to call m68k_set_illg_instr_callback explicitely
+ */
+#define M68K_ILLG_HAS_CALLBACK	    OPT_OFF
+#define M68K_ILLG_CALLBACK(opcode)  op_illg(opcode)
 
 /* If ON, CPU will call the set fc callback on every memory access to
  * differentiate between user/supervisor, program/data access like a real
@@ -121,22 +150,20 @@
 #define M68K_EMULATE_FC             OPT_OFF
 #define M68K_SET_FC_CALLBACK(A)     your_set_fc_handler_function(A)
 
-
 /* If ON, CPU will call the pc changed callback when it changes the PC by a
  * large value.  This allows host programs to be nicer when it comes to
  * fetching immediate data and instructions on a banked memory system.
  */
-#define M68K_MONITOR_PC             OPT_SPECIFY_HANDLER
-#define M68K_SET_PC_CALLBACK(A)     m68k_pc_changed_handler_function(A)
-void m68k_pc_changed_handler_function(unsigned int addr);
+#define M68K_MONITOR_PC             OPT_OFF
+#define M68K_SET_PC_CALLBACK(A)     your_pc_changed_handler_function(A)
+
 
 /* If ON, CPU will call the instruction hook callback before every
  * instruction.
  */
-//#define M68K_INSTRUCTION_HOOK       OPT_SPECIFY_HANDLER
 #define M68K_INSTRUCTION_HOOK       OPT_OFF
-#define M68K_INSTRUCTION_CALLBACK() m68k_instruction()
-void m68k_instruction();
+#define M68K_INSTRUCTION_CALLBACK(pc) your_instruction_hook_function(pc)
+
 
 /* If ON, the CPU will emulate the 4-byte prefetch queue of a real 68000 */
 #define M68K_EMULATE_PREFETCH       OPT_OFF
@@ -155,8 +182,11 @@ void m68k_instruction();
  */
 #define M68K_LOG_ENABLE             OPT_OFF
 #define M68K_LOG_1010_1111          OPT_OFF
-#define M68K_LOG_FILEHANDLE         stderr
+#define M68K_LOG_FILEHANDLE         some_file_handle
 
+/* Emulate PMMU : if you enable this, there will be a test to see if the current chip has some enabled pmmu added to every memory access,
+ * so enable this only if it's useful */
+#define M68K_EMULATE_PMMU   OPT_OFF
 
 /* ----------------------------- COMPATIBILITY ---------------------------- */
 
@@ -171,57 +201,10 @@ void m68k_instruction();
 #define M68K_USE_64_BIT  OPT_OFF
 
 
-/* Set to your compiler's static inline keyword to enable it, or
- * set it to blank to disable it.
- * If you define INLINE in the makefile, it will override this value.
- * NOTE: not enabling inline functions will SEVERELY slow down emulation.
- */
-#ifndef INLINE
-#define INLINE static __inline__
-#endif /* INLINE */
-
 #endif /* M68K_COMPILE_FOR_MAME */
-
 
 /* ======================================================================== */
 /* ============================== END OF FILE ============================= */
 /* ======================================================================== */
-
-
-#include <byteswap.h>
-#include <stdint.h>
-
-extern unsigned char *m68k_pcbase;
-
-static inline unsigned int m68k_read_immediate_16(unsigned int address) {
-	address&=0xFFFFFF;
-	uint16_t *p=(uint16_t*)(m68k_pcbase+address);
-	return __bswap_16(*p);
-}
-
-static inline unsigned int m68k_read_immediate_32(unsigned int address) {
-	address&=0xFFFFFF;
-	uint32_t *p=(uint32_t*)(m68k_pcbase+address);
-	return __bswap_32(*p);
-}
-
-static inline unsigned int  m68k_read_pcrelative_8(unsigned int address) {
-	address&=0xFFFFFF;
-	uint8_t *p=(uint8_t*)(m68k_pcbase+address);
-	return *p;
-}
-
-static inline unsigned int  m68k_read_pcrelative_16(unsigned int address) {
-	address&=0xFFFFFF;
-	uint16_t *p=(uint16_t*)(m68k_pcbase+address);
-	return __bswap_16(*p);
-}
-
-static inline unsigned int  m68k_read_pcrelative_32(unsigned int address) {
-	address&=0xFFFFFF;
-	uint32_t *p=(uint32_t*)(m68k_pcbase+address);
-	return __bswap_32(*p);
-}
-
 
 #endif /* M68KCONF__HEADER */
